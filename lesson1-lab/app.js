b5fae87f1f7a0cf2b954a015eebd9742bb446761
john katua <johnkatua99@gmail.com>
2023-05-14 05:13:48 +0300

    changed audio file to base64 for data  analysis

--- lesson1-lab/app.js
+++ lesson1-lab/app.js
@@ -1,6 +1,30 @@
 const btn1 = document.getElementById('button1');
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
 
-let audio1 = new Audio();
-audio1.src = './sounds/explode.wav';
+console.log(audioCtx);
 
-btn1.addEventListener('click', () => audio1.play());
+let audio1 = null;
+fetch('audio.txt')
+  .then(response => response.text())
+  .then(text => {
+    audio1 = new Audio(text);
+  });
+// audio1.src = './sounds/explode.wav';
+
+btn1.addEventListener('click', () => {
+  audio1.play();
+  audio1.addEventListener('play', () => {
+    console.log('play');
+  });
+  audio1.addEventListener('pause', () => {
+    console.log('pause');
+  });
+  audio1.addEventListener('ended', () => {
+    console.log('ended');
+  });
+});
+
+const btn2 = document.getElementById('button2');
+btn2.addEventListener('click', () => {
+  audio1.pause();
+});
